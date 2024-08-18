d46609d6da6683951b26bd1d78ee796311f4ac85
Boezienko <boeazienko@gmail.com>
2024-08-18 14:51:57 -0500

    Revert "Update MainActivity.kt and add profile_picture.png"
    This reverts commit 2e33a8a479b85e62c758087035d6d40f5961d6bf.

--- Android/JetpackComposeTutorial/app/src/main/java/com/morning/composetutorial/MainActivity.kt
+++ Android/JetpackComposeTutorial/app/src/main/java/com/morning/composetutorial/MainActivity.kt
@@ -3,16 +3,13 @@ package com.morning.composetutorial
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.morning.composetutorial.ui.theme.ComposeTutorialTheme
 
@@ -29,23 +26,10 @@ class MainActivity : ComponentActivity() {
     // composable function to display text
     @Composable
     fun MessageCard(msg: Message) {
-        Row{
-            // makes image show up in a row next to the column element
-            Image(
-                painter = painterResource(R.drawable.profile_picture),
-                contentDescription = "Contact profile picture"
-            )
-
-            // makes inside elements stack on top of one another
-            Column {
-                Text(text = msg.author)
-                Text(text = msg.body)
-            }
-
+        Column {
+            Text(text = msg.author)
+            Text(text = msg.body)
         }
-
-
-
     }
 
     // preview composable function to show composable function in design window
@@ -54,4 +38,7 @@ class MainActivity : ComponentActivity() {
     fun PreviewMessageCard(){
         MessageCard(Message("Lexi", "Hey, take a look at Jetpack Compose, it's great!"))
     }
+
+
+
 }
