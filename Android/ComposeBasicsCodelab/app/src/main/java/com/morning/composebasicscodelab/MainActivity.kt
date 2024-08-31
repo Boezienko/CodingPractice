601bc70193f6a240cb8635f6b9b4d92c491f84dd
Boezienko <boeazienko@gmail.com>
2024-08-30 21:19:38 -0500

    Edit MainActivity.kt and strings.xml
    Made finishing touches to app and put strings in the xml file so they wouldn't be hard coded in

--- Android/ComposeBasicsCodelab/app/src/main/java/com/morning/composebasicscodelab/MainActivity.kt
+++ Android/ComposeBasicsCodelab/app/src/main/java/com/morning/composebasicscodelab/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.spring
@@ -15,8 +16,15 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -28,6 +36,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -45,58 +54,12 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    // used by instead of = so when we use expanded we don't have to use expanded.value
-    var expanded by rememberSaveable { mutableStateOf(false)}
-    val extraPadding by animateDpAsState(
-        if (expanded) 48.dp else 0.dp,
-        animationSpec = spring( // this parameter lets you customize the animation
-            dampingRatio = Spring.DampingRatioMediumBouncy,
-            stiffness = Spring.StiffnessLow
-        ),
-        label = "Greeting expansion"
-    )
-    Surface(
-            color = MaterialTheme.colorScheme.primary,
-            modifier = modifier.padding(
-                vertical = 4.dp,
-                horizontal = 8.dp
-            )
-        ) {
-        Row(modifier = Modifier.padding(24.dp)) {
-            Column(
-                modifier = modifier
-                    .weight(1f)
-                    // ensuring the padding is never negative because that could crash the app
-                    .padding(bottom = extraPadding.coerceAtLeast(0.dp))
-            ){
-                Text(text = "Hello ")
-                // used material theme to style this text
-                Text(
-                    text = name,
-                    style = MaterialTheme.typography.headlineMedium.copy(
-                        fontWeight = FontWeight.ExtraBold
-                    )
-                )
-            }
-            ElevatedButton(
-                onClick = { expanded = !expanded }
-            ) {
-                Text(if (expanded) "Show less" else "Show more")
-            }
-        }
-
-    }
-
-}
-
-@Composable
 fun MyApp(modifier: Modifier = Modifier) {
 
-    // we are hoisting this state from the OnboardingScreen to the MyApp
+    // to hold the state of if the onboarding is showing or not
     var shouldShowOnboarding by rememberSaveable { mutableStateOf(true) }
 
-    Surface(modifier) {
+    Surface(modifier, color = MaterialTheme.colorScheme.background) {
         if (shouldShowOnboarding) {
             OnboardingScreen( onContinueClicked = { shouldShowOnboarding = false })
         } else {
@@ -137,27 +100,60 @@ private fun Greetings(
     }
 }
 
-@Preview
 @Composable
-fun MyAppPreview() {
-    ComposeBasicsCodelabTheme {
-        MyApp(Modifier.fillMaxSize())
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Card(
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.primary
+        ),
+        modifier = modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+        CardContent(name)
     }
 }
 
-@Preview(showBackground = true, widthDp = 320, heightDp = 320)
 @Composable
-fun OnboardingPreview() {
-    ComposeBasicsCodelabTheme {
-        OnboardingScreen(onContinueClicked = {}) // o nothing on click
-    }
-}
+private fun CardContent(name: String){
+    var expanded by rememberSaveable { mutableStateOf(false) }
 
-@Preview(showBackground = true, widthDp = 320)
-@Composable
-fun GreetingsPreview(){
-    ComposeBasicsCodelabTheme {
-        Greetings()
+    Row(
+        modifier = Modifier
+            .padding(12.dp)
+            .animateContentSize(
+                animationSpec = spring(
+                    dampingRatio = Spring.DampingRatioMediumBouncy,
+                    stiffness = Spring.StiffnessLow
+                )
+            )
+    ) {
+        Column(
+            modifier = Modifier
+                .weight(1f)
+                .padding(12.dp)
+        ) {
+            Text(text = "Hello, ")
+            Text(
+                text = name, style = MaterialTheme.typography.headlineMedium.copy(
+                    fontWeight = FontWeight.ExtraBold
+                )
+            )
+            if (expanded) {
+                Text(
+                    text = ("Composem ipsum color sit lazy, " +
+                        "padding theme elit, sed do bouncy. ").repeat(4))
+            }
+        }
+
+        IconButton(onClick = { expanded = !expanded }) {
+            Icon(
+                imageVector = if (expanded) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
+                contentDescription = if (expanded) {
+                    stringResource(R.string.show_less)
+                } else {
+                    stringResource(R.string.show_more)
+                }
+            )
+        }
     }
 }
 
@@ -174,3 +170,23 @@ fun GreetingPreview() {
         MyApp()
     }
 }
+
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
+@Composable
+fun OnboardingPreview() {
+    ComposeBasicsCodelabTheme {
+        OnboardingScreen(onContinueClicked = {}) // do nothing on click
+    }
+}
+
+@Preview
+@Composable
+fun MyAppPreview() {
+    ComposeBasicsCodelabTheme {
+        MyApp(Modifier.fillMaxSize())
+    }
+}
+
+
+
+
