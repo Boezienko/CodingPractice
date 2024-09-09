6e1029987f596ae1c28d989b66389b7ccbd6652a
Boezienko <boeazienko@gmail.com>
2024-09-09 15:16:09 -0500

    Made video player
    Made the video player
    Upon running the app, it plays a video and users are able to pause/play/fastforward the video

--- Android/video_player_MetaCourse/app/src/main/java/com/example/video_player/MainActivity.kt
+++ Android/video_player_MetaCourse/app/src/main/java/com/example/video_player/MainActivity.kt
@@ -1,5 +1,10 @@
 package com.example.video_player
 
+import android.R
+import android.net.Uri
+import android.net.Uri.*
+import android.widget.MediaController
+import android.widget.VideoView
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.ui.AppBarConfiguration
@@ -14,5 +19,25 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        // get the video view we made in activity_main.xml
+        val videoView = findViewById<VideoView>(binding.testView.id)
+
+        // create media controller and anchor it to the video
+        val mediaController = MediaController(this)
+        mediaController.setAnchorView(videoView)
+
+        // specify location of video file
+        val uri:Uri = parse(
+            "android.resource://" +
+            packageName +
+            "/raw/test"
+        )
+
+        // setting mediaController and URI, then starting the videoView
+        videoView.setMediaController(mediaController)
+        videoView.setVideoURI(uri)
+        videoView.requestFocus()
+        videoView.start()
     }
 }
